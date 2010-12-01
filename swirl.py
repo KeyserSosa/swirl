53d86ab78df99dea8394739715dc81a968675a3b
Chris Slowe <chris@hipmunk.com>
2010-12-01 12:02:28 -0800

    added ability to have swirled functions also return an async result up the chain.  Special Return object breaks the iteration and allows passing up the stack

--- swirl.py
+++ swirl.py
@@ -15,24 +15,30 @@ try:
 except ImportError:
     # Python < 2.6
     def isgeneratorfunction(obj):
-        return bool((inspect.isfunction(object) or inspect.ismethod(object)) and
-                    obj.func_code.co_flags & CO_GENERATOR)
+        return bool((inspect.isfunction(object) or
+                     inspect.ismethod(object)) and
+                    obj.func_code.co_flags & inspect.CO_GENERATOR)
 
 __version__ = '0.1.1'
 
+
 class CoroutineRunner(object):
-    def __init__(self, generator, web_handler=None, io_loop=None):
+    def __init__(self, generator, web_handler=None, io_loop=None,
+                 callback = None):
         self.gen = generator
         self.web_handler = web_handler
         self.io_loop = io_loop
-        
+        self.work = None
+
+        self.final_callback = callback
+
         # start the ball rolling...
         self.callback_proxy()
-    
+
     def execute_work(self):
         return self.work(self.callback_proxy)
 
-    def callback_proxy(self, *args):
+    def callback_proxy(self, *args, **kwargs):
         try:
             if len(args) > 0:
                 if isinstance(args[-1], Exception):
@@ -49,14 +55,23 @@ class CoroutineRunner(object):
                         self.work = self.gen.send(args)
             else:
                 self.work = self.gen.next()
-            
+
+            if isinstance(self.work, YieldReturn):
+                raise StopIteration
+
             if self.io_loop is None:
                 self.io_loop = IOLoop.instance()
-            
+
             self.io_loop.add_callback(self.execute_work)
         except StopIteration:
+
             if self.web_handler and not self.web_handler._finished:
                 self.web_handler.finish()
+
+            if (isinstance(self.work, YieldReturn) and
+                self.final_callback is not None):
+                self.work(self.final_callback)
+
         except Exception, e:
             if self.web_handler:
                 if self.web_handler._headers_written:
@@ -67,18 +82,38 @@ class CoroutineRunner(object):
             else:
                 raise
 
+
+def return_(val):
+    return YieldReturn(val)
+
+
+class YieldReturn(object):
+    def __init__(self, x):
+        self.res = x
+
+    def __call__(self, callback):
+        if ((inspect.isfunction(self.res) or inspect.ismethod(self.res)) and
+            getattr(self.res, "is_callback_wrapper", False)):
+            self.res(callback)
+        else:
+            return callback(self.res)
+
+    def __repr__(self):
+        return "<Yielded '%r'>" % self.res
+
+
 def make_asynchronous_decorator(io_loop):
     """
     Creates an asynchronous decorator that uses the given I/O loop.
-    
+
     If the `io_loop` argument is None, the default IOLoop instance will be
     used.
-    
+
     For information on how to use such a decorator, see
     `swirl.asynchronous`.
     """
-    
-    def asynchronous(coroutine):
+
+    def asynchronous(coroutine, callback = None):
         """
         Allows a function to not use explicit callback functions to respond
         to asynchronous events.
@@ -87,21 +122,51 @@ def make_asynchronous_decorator(io_loop):
         if not isgeneratorfunction(coroutine):
             # the "coroutine" isn't actually a coroutine; just return
             # its result like tornado.web.asynchronous would do
-            return coroutine
-        
+            if callback is not None:
+                return callback(coroutine)
+            else:
+                return coroutine
+
         web_async_coroutine = web_async(coroutine)
-        
+
         @functools.wraps(coroutine)
         def run_async_routine(*args, **kwargs):
             # we check if we're an instancemethod of RequestHandler for better
             # intergration
             if len(args) > 0 and isinstance(args[0], RequestHandler):
                 CoroutineRunner(web_async_coroutine(*args, **kwargs),
-                    args[0], io_loop)
+                    args[0], io_loop, callback = callback)
             else:
-                CoroutineRunner(coroutine(*args, **kwargs), io_loop=io_loop)
-        
-        return run_async_routine    
+                CoroutineRunner(coroutine(*args, **kwargs), io_loop=io_loop,
+                                callback = callback)
+
+        return run_async_routine
+
     return asynchronous
 
 asynchronous = make_asynchronous_decorator(None)
+
+
+def make_async_with_return(ioloop):
+    _asynchronous = make_asynchronous_decorator(ioloop)
+    def with_callback(func):
+
+        if not isgeneratorfunction(func):
+            raise ValueError, "can't async a func with no yield"
+
+        @functools.wraps(func)
+        def _with_callback(*args, **kwargs):
+
+            def _partial_call(callback):
+
+                f = _asynchronous(func, callback = callback)
+                return f(*args, **kwargs)
+
+            _partial_call.is_callback_wrapper = True
+            return _partial_call
+
+        _with_callback.has_callback_wrapper = True
+        return _with_callback
+    return with_callback
+
+async_return = make_async_with_return(None)
